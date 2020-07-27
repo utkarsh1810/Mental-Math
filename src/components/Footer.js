808e2be9842400f0d7ce74bb2f98bf1ff6408397
Utkarsh <utkarshverma782@gmail.com>
2020-07-27 22:34:14 +0530

    adding footer of the application

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+        <div className="col-12">
+          <div className="footer p-2 mt-4 mx-0 text-center bg-dark text-light">
+            Developed by :  
+            <span className="text-warning ">
+              Utkarsh  
+            </span>
+            , using  <i className="fab fa-react"/> React js &amp; Bootstrap 
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
